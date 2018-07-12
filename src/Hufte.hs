786e85406722b4fb7c5598d4182b059a837ca66e
Mladen Srdić <m.s.qubits@gmail.com>
2018-07-12 23:06:55 +0200

    Still playing around...

--- src/Hufte.hs
+++ src/Hufte.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Hufte (barSparkline, defaultConf) where
+module Hufte (barSparkline, defaultConf, height) where
 
 import           Codec.Binary.Base64 (encode)
 import           Data.ByteString     (unpack)
@@ -20,7 +20,7 @@ data SparklineConf =
                 , markCurr  :: Bool
                 }
 
-type Values = [Double]
+type Values = [Int]
 
 main = do
   print "I am Hufte."
@@ -30,10 +30,11 @@ dataURI :: Image -> IO String
 dataURI image =
   fmap (encode . unpack) (savePngByteString image)
 
-transparent = rgba 0xff 0xff 0xff 0xff
+transparent = rgba 0xff 0xff 0xff 0x00
 defaultGrey = rgb 0xde 0xde 0xde
 defaultRed = rgb 0xff 0x0d 0x0d
 defaultGreen = rgb 0x0d 0xff 0x0d
+black = rgb 0x00 0x00 0x00
 
 defaultConf :: SparklineConf
 defaultConf = SparklineConf { width = 120
@@ -53,11 +54,15 @@ barSparkline conf vs = do
   let nPoints  = length vs
       w        = width conf
       h        = height conf
-      stepSize = fromIntegral (w - nPoints) / fromIntegral nPoints
-  print stepSize
+      stepSize = fromIntegral (w - nPoints) `div` (fromIntegral nPoints - 1)
 
   img <- newImage (w, h)
-  drawFilledRectangle (0, 0) (w, h) transparent img
-  drawFilledRectangle (w `div` 4,h `div` 4) (3*w `div` 4, 3*h `div` 4) defaultGrey img
+  drawFilledRectangle (0, 0) (w, h) (bgColor conf) img
+  let points = zip [0, stepSize ..] vs
+      lines' = zip points (tail points)
+      drawLine' c i (p1, p2) = antiAliased (drawLine p1 p2) c i
+
+  mapM_ (drawLine' (currColor conf) img) lines'
+
   byteEncoded <- dataURI img
   return $ "<img src='data:image/png;base64," ++ byteEncoded ++ "'>"
--- src/WebTest.hs
+++ src/WebTest.hs
@@ -8,11 +8,30 @@ import           Data.Monoid            (mconcat)
 
 import           Hufte
 
+import           Control.Monad          (replicateM)
 import           Control.Monad.IO.Class (liftIO)
 
 import           Data.Text.Lazy         (pack)
 
+import           System.Random          (newStdGen, randomRs)
+
 main = scotty 3000 $
   get "/" $ do
-    sparkline <- liftIO $ barSparkline defaultConf []
-    html $ mconcat $ (mconcat .replicate 30) ["<p>", pack sparkline, "</p>"]
+    points <- param "points"
+    graphs <- param "graphs"
+
+    let pn = read points :: Int
+    let gn = read graphs :: Int
+
+    sparklines <- liftIO $ randomSparklines gn pn
+    html $ mconcat $ map toParagraph sparklines
+      where toParagraph sparkline = mconcat ["<span>", pack sparkline, "</span>"]
+
+randomSparkline :: Int -> IO String
+randomSparkline pn = do
+  gen <- newStdGen
+  let randomVals = take pn $ randomRs (0, height defaultConf) gen
+  liftIO $ barSparkline defaultConf randomVals
+
+randomSparklines :: Int -> Int -> IO [String]
+randomSparklines gn pn = replicateM gn $ randomSparkline pn
